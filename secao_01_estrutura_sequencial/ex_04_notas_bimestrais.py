c3ccb5a43a5f85bade7e8edcae7b504f3c9556d6
saiayla <lailacristinaaraujorocha2@gmail.com>
2025-04-02 13:23:57 -0300

    Corrigindo o exercício 05 da seção 01

--- secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
+++ secao_01_estrutura_sequencial/ex_04_notas_bimestrais.py
@@ -13,20 +13,22 @@ Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 """
 
 
-def calcular_media():
+def calcular_media(notas):
     """Escreva aqui em baixo a sua solução"""
-    while True:
-        try:
-            n1 = float(input("Insira a nota do primeiro bimestre: "))
-            n2 = float(input("Insira a nota do segundo bimestre: "))
-            n3 = float(input("Insira a nota do terceiro bimestre: "))
-            n4 = float(input("Insira a nota do quarto bimestre: "))
-            break
-        except ValueError:
-            print("Insira notas válidas!")
+    return sum(notas) / len(notas)
 
-    notas = [n1, n2, n3, n4]
-    media = sum(notas) / len(notas)
-    print(f"A média anual é {media:.2f}")
+while True:
+    try:
+        n1 = float(input("Insira a nota do primeiro bimestre: "))
+        n2 = float(input("Insira a nota do segundo bimestre: "))
+        n3 = float(input("Insira a nota do terceiro bimestre: "))
+        n4 = float(input("Insira a nota do quarto bimestre: "))
+        break
+    except ValueError:
+        print("Insira notas válidas!")
+
+notas = [n1, n2, n3, n4]
+media = calcular_media(notas)
+print(f"A média anual é {media:.1f}")
 
 
