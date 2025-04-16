2a5691b2420bea36ab0c9536e2d46cfb0bb710eb
saiayla <lailacristinaaraujorocha2@gmail.com>
2025-04-16 10:24:04 -0300

    Resolução do exercício 01 da seção 02

--- secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
+++ secao_02_estrutura_de_decisao/ex_01_imprimir_maior_numero.py
@@ -17,3 +17,18 @@ Faça um Programa que peça dois números e imprima o maior deles.
 
 def maior_de_dois_numeros(x, y):
     """Escreva aqui em baixo a sua solução"""
+    if x > y:
+        print(x)
+    elif y > x:
+        print(y)
+    else:
+        print("Os dois números são iguais.")
+
+while True:
+    try:
+        x = float(input("Insira um número: "))
+        y = float(input("Insira outro número: "))
+        break
+    except ValueError:
+        print("Insira um número válido!")
+maior_de_dois_numeros(x, y)
