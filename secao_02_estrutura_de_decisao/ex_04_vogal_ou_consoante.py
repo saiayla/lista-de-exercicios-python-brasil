58072555167dcad0da142a4d5cdb3addf6305327
saiayla <lailacristinaaraujorocha2@gmail.com>
2025-04-16 11:02:42 -0300

    Resolução do exercício 04 da seção 02

--- secao_02_estrutura_de_decisao/ex_04_vogal_ou_consoante.py
+++ secao_02_estrutura_de_decisao/ex_04_vogal_ou_consoante.py
@@ -17,3 +17,16 @@ Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
 def vogal_ou_consoante(letra):
     """Escreva aqui em baixo a sua solução"""
+    vogais = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'o', 'u']
+    if letra in vogais:
+        print("Vogal")
+    else:
+        print("Consoante")
+
+while True:
+    letra = input("Insira uma letra: ")
+    if letra.isalpha():
+        break
+    else:
+        print("Insira uma letra válida!")
+vogal_ou_consoante(letra)
