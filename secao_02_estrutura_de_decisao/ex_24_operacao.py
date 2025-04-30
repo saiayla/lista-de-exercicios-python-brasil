8622c7c4cec589c925c83d187f3275ee00e8cd05
saiayla <lailacristinaaraujorocha2@gmail.com>
2025-04-30 14:02:52 -0300

    Resolução do exercício 24 da seção 02

--- secao_02_estrutura_de_decisao/ex_24_operacao.py
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -30,4 +30,50 @@ Mostre o restultado com duas casas decimais
 
 
 def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
-    """Escreva aqui em baixo a sua solução"""
+  """Escreva aqui em baixo a sua solução"""
+  match (operacao):
+    case 'Soma'|'soma':
+      resultado = n_1 + n_2
+    case 'Subtração'|'subtração':
+      resultado = n_1 - n_2
+    case 'Multiplicação'|'multiplicação':
+      resultado = n_1 * n_2
+    case 'Divisão'|'divisão':
+      resultado = n_1 / n_2
+    case _:
+      print("Operação inválida!")
+      
+  def par_ou_impar(resultado):
+    if resultado % 2 == 0:
+      return 'par'
+    else:
+      return 'ímpar'
+    
+  def positivo_ou_negativo(resultado):
+    if resultado > 0:
+      return 'positivo'
+    elif resultado < 0:
+      return 'negativo'
+    else:
+      return 'netro'
+    
+  def inteiro_ou_decimal(resultado):
+    if resultado == round(resultado):
+      return 'inteiro'
+    else:
+      return 'decimal'
+  
+  print(f"Resultado: {resultado:.2f}\nNúmero é {par_ou_impar(resultado)}, {positivo_ou_negativo(resultado)} e {inteiro_ou_decimal(resultado)}.")
+    
+while True:
+  try:
+    n1 = float(input("Insira o primeiro número: "))
+    n2 = float(input("Insira o segundo número: "))
+    break
+  
+  except ValueError:
+    print("Insira um número válido!")
+
+operacao = input("Escolha uma operação (soma, subtração, multiplicação, ou divisão): ")
+fazer_operacao_e_classificar(n1, n2, operacao)
+
