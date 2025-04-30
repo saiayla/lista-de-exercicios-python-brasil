8d2961aa84f1f341bcd2ed6897c13b243223605c
saiayla <lailacristinaaraujorocha2@gmail.com>
2025-04-30 14:56:49 -0300

    Resolução do exercício 26 da seção 02

--- secao_02_estrutura_de_decisao/ex_26_posto_de_gasolina.py
+++ secao_02_estrutura_de_decisao/ex_26_posto_de_gasolina.py
@@ -31,7 +31,44 @@ Mostre o restultado com duas casas decimais
     '30 litro(s) de gasolina custa(m): R$ 75.00. Com 6% de desconto, fica R$ 70.50'
 
 """
-
-
 def calcular_abastecimento(litros_de_combustivel: float, tipo_de_combustivel: str) -> str:
     """Escreva aqui em baixo a sua solução"""
+    match(tipo_de_combustivel):
+        case 'a'|'A':
+            tipo_de_combustivel = 'álcool'
+            preco = litros_de_combustivel * 1.9
+            if litros_de_combustivel <= 20:
+                d = '3%'
+                desconto = 1.9 - (0.03 * 1.9)
+                preco_desconto = preco - desconto
+            elif litros_de_combustivel > 20:
+                d = '5%'
+                desconto = 1.9 - (0.05 * 1.9)
+                preco_desconto = preco - desconto
+                
+        case 'g'|'G':
+            tipo_de_combustivel = 'gasolina'
+            preco = litros_de_combustivel * 2.5
+            if litros_de_combustivel <= 20:
+                d = '4%'
+                desconto = 1.9 - (0.04 * 2.5)
+                preco_desconto = preco - desconto
+            elif litros_de_combustivel > 20:
+                d = '6%'
+                desconto = 1.9 - (0.06 * 2.5)
+                preco_desconto = preco - desconto
+        case _:
+            print("Entrada inválida!")
+                
+    print(f"{litros_de_combustivel} litro(s) de {tipo_de_combustivel} custa(m): R$ {preco:.2f}. Com {d} de desconto, fica R$ {preco_desconto:.2f}")
+    
+while True:
+    try:
+        litros = int(input("Quantidade de litros vendidos: "))
+        break
+    except ValueError:
+        print("Insira uma quantidade válida!")
+        
+combustivel = input("Qual o tipo de combustível foi vendido (A-álcool, G-gasolina)? ")
+calcular_abastecimento(litros, combustivel)
+
